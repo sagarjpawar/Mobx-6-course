cc3310b0ce77907a3e46355cba53147f53605674
Sagar Pawar <sagar.pawar@synerzip.com>
2020-12-16 22:49:12 +0530

    makeAutoObservable

--- src/index.js
+++ src/index.js
@@ -4,6 +4,74 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {autorun, makeAutoObservable, observable, computed, action} from 'mobx'
+
+let runningId = 0;
+// class Person{
+//   id = runningId++;
+
+//   name;
+//   constructor(name){
+//     this.name = name
+
+//     makeAutoObservable(this,{
+//       id: false
+//     })
+
+//     // makeObservable(this, {
+//     //   name: observable,
+//     //   updateName: action,
+//     //   coolName: computed
+//     // })
+    
+//   }
+//   updateName(name){
+//     this.name = name
+//   }
+
+//   get coolName() {
+//     return `${this.name} is Cool`
+//   }
+// }
+class Person{
+  id = runningId++;
+  
+  @observable
+  name;
+  constructor(name){
+    this.name = name
+
+    makeAutoObservable(this,{
+      id: false
+    })
+
+    // makeObservable(this, {
+    //   name: observable,
+    //   updateName: action,
+    //   coolName: computed
+    // })
+    
+  }
+
+  @action
+  updateName(name){
+    this.name = name
+  }
+
+  @computed
+  get coolName() {
+    return `${this.name} is Cool`
+  }
+}
+
+const person = new Person('sagar')
+
+autorun(()=>{
+  console.log(`${person.coolName}`)
+})
+
+person.updateName('Sagar Pawar')
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
